5ed69a4731f09bde7344f902e471fdee30a7029f
Alexey Tkachenko <iskaspb@gmail.com>
2021-08-16 05:04:38 +0800

    improve optimizer script

--- src/strategy/optimize_MACD.py
+++ src/strategy/optimize_MACD.py
@@ -24,25 +24,24 @@ def main() -> None:
 
     macdParams = GenerateMACDParams()
     #macdParams = macdParams[0:1]
-    print(f"MACD parameters generated : {len(macdParams)}")
+    print(f"#MACD parameters generated : {len(macdParams)}")
     symbols = sorted(set(["AAPL", "BABA", "TSLA", "INTC", "NVDA", "MU", "FB", "WMT", "AMD", "AMZN", "GOOG", "HP", "GE", "F", "T",  "BAC", "CSCO", "KO", "PINS", "PG", "PEP", "UPS", "PYPL"]))
-    print(f"Optimize for {len(symbols)} symbols : {symbols}")
+    print(f"#Optimize for {len(symbols)} symbols : {symbols}")
 
     for symbolIndex in range(0, len(symbols)-1):
         startOptimize = timer()
 
         symbol = symbols[symbolIndex]
-        #optimizeCmd = ["lean", "optimize", "--strategy", 'Grid Search', "--target", 'Sharpe Ratio', "--target-direction", "max", "--parameter", "macd-param-index", "0", f"{len(macdParams)-1}", "1", "--parameter", "symbol-index", f"{symbolIndex}", f"{symbolIndex}", "1", "MACD_Simple"]
         optimizeCmd = ["lean", "optimize", "--strategy", "Grid Search", "--target", "Compounding Annual Return", "--target-direction", "max", "--constraint", "Sharpe Ratio >= 1.5", "--parameter", "macd-param-index", "0", f"{len(macdParams)-1}", "1", "--parameter", "symbol-index", f"{symbolIndex}", f"{symbolIndex}", "1", "MACD_Simple"]
-        print(f"#optimize for symbol {symbol} : {' '.join(optimizeCmd)}", flush=True)
+        print(f"#optimize for symbol {symbol} using command : {' '.join(optimizeCmd)}", flush=True)
         process = subprocess.Popen(optimizeCmd, stdout=subprocess.PIPE)
         output, error = process.communicate()
         output = output.decode('utf-8', 'ignore')[-3000:]
 
         x = re.search(r"Optimization has ended. Result was not", output)
         if x:
-            print(f"#Optimization of {symbol} failed, please review optimization criterias")
-            print(output)
+            print(f"#Optimization of {symbol} failed, please review optimization criterias - you can repeate the optimization command above to get more insights")
+            #print(output)
             continue
 
         x = re.search(r"Optimal parameters: symbol-index: (\d+), macd-param-index: (\d+)", output)
@@ -56,7 +55,7 @@ def main() -> None:
             slow = resultMACDParam[1]
             signal = resultMACDParam[2]
         else:
-            print(f"Couldn't interpret the output symbol {symbol}")
+            print(f"ERROR: Couldn't interpret the output symbol {symbol}")
             print(output)
             continue
 
@@ -64,7 +63,7 @@ def main() -> None:
         if x:
             annualInterest = float(x.group(1))
         else:
-            print(f"Couldn't interpret the output symbol {symbol}")
+            print(f"ERROR: Couldn't interpret the output symbol {symbol}")
             print(output)
             continue
 
@@ -72,7 +71,7 @@ def main() -> None:
         if x:
             sharpeRatio = float(x.group(1))
         else:
-            print(f"Couldn't interpret the output symbol {symbol}")
+            print(f"ERROR: Couldn't interpret the output symbol {symbol}")
             print(output)
             continue
 
@@ -80,16 +79,16 @@ def main() -> None:
         if x:
             totalTrades = int(x.group(1))
         else:
-            print(f"Couldn't interpret the output symbol {symbol}")
+            print(f"ERROR: Couldn't interpret the output symbol {symbol}")
             print(output)
             continue
 
-        print(f'self.indicators["{symbol}"] = {{"MACD": self.MACDIndicator(self, "{symbol}", {fast}, {slow}, {signal})}} #Total Trades: {totalTrades}, Compounding Annual Return: {annualInterest}, Sharpe Ratio: {sharpeRatio}', flush=True)
+        print(f'self.MACDIndicator("{symbol}", {fast}, {slow}, {signal}, interval) #Total Trades: {totalTrades}, Compounding Annual Return: {annualInterest}, Sharpe Ratio: {sharpeRatio}', flush=True)
         elapsedOptimizeTime = timedelta(seconds=timer()-startOptimize)
         print(f"#Elapsed optimize {symbol} MACD iteration time : {elapsedOptimizeTime}")
 
     elapsedTotalTime = timedelta(seconds=timer() - start)
-    print(f"Elapsed optimize total time : {elapsedTotalTime}")
+    print(f"#Elapsed optimize total time : {elapsedTotalTime}")
 
 
 
